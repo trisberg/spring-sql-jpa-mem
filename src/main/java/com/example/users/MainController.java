e754368e0ca7f1220921c67def59f16427b3b8c4
jmconelogin <98829230+jmconelogin@users.noreply.github.com>
2022-03-02 16:11:42 -0500

    Update MainController.java
    Add notification service method endpoint

--- src/main/java/com/example/users/MainController.java
+++ src/main/java/com/example/users/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.cvs.notification.NotificationRequest;
 
 @Controller	// This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
@@ -20,7 +21,6 @@ public class MainController {
 			, @RequestParam String email) {
 		// @ResponseBody means the returned String is the response, not a view name
 		// @RequestParam means it is a parameter from the GET or POST request
-
 		User n = new User();
 		n.setName(name);
 		n.setEmail(email);
@@ -31,6 +31,14 @@ public class MainController {
 	@GetMapping(path="/all")
 	public @ResponseBody Iterable<User> getAllUsers() {
 		// This returns a JSON or XML with the users
+
 		return userRepository.findAll();
+	}
+
+	@PostMapping(path="/refill")
+	public @ResponseBody String refill(@RequestParam String userEmail) {
+		NotificationClient client = new NotificationClient();
+		client.callNotification(userEmail, "Refill");
+		return "";
 	}
 }
